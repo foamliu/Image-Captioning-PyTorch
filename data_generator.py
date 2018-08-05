3b76cc55ac82fb698aeab449320e967a93ea2526
Yang Liu <foamliu@yeah.net>
2018-08-05 15:16:18 +0800

    update

--- data_generator.py
+++ data_generator.py
@@ -1,29 +1,27 @@
 # encoding=utf-8
 import os
 import pickle
-import random
 
 import cv2 as cv
 import keras
 import numpy as np
-from keras.applications.resnet50 import preprocess_input
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.models import Model
 from keras.preprocessing import sequence
 from keras.utils import Sequence
 
-from config import batch_size, max_token_length, vocab_size, train_image_folder, valid_image_folder, img_size, channel
-
-
-def random_crop(image):
-    full_size = image.shape[0]
-    u = random.randint(0, full_size - img_size)
-    v = random.randint(0, full_size - img_size)
-    image = image[v:v + img_size, u:u + img_size]
-    return image
+from config import batch_size, max_token_length, vocab_size, train_image_folder, valid_image_folder, img_size
 
 
 class DataGenSequence(Sequence):
     def __init__(self, usage):
         self.usage = usage
+
+        image_encoder = VGG16(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet',
+                              pooling=None)
+        image_input = image_encoder.layers[0].input
+        x = image_encoder.layers[-2].output
+        self.image_model = Model(inputs=image_input, outputs=x)
 
         vocab = pickle.load(open('data/vocab_train.p', 'rb'))
         self.idx2word = sorted(vocab)
@@ -32,8 +30,10 @@ class DataGenSequence(Sequence):
         if usage == 'train':
             samples_path = 'data/samples_train.p'
             self.image_folder = train_image_folder
+            self.image_folder = train_image_folder
         else:
             samples_path = 'data/samples_valid.p'
+            self.image_folder = valid_image_folder
             self.image_folder = valid_image_folder
 
         samples = pickle.load(open(samples_path, 'rb'))
@@ -47,8 +47,8 @@ class DataGenSequence(Sequence):
         i = idx * batch_size
 
         length = min(batch_size, (len(self.samples) - i))
-        batch_image_input = np.empty((length, img_size, img_size, channel), dtype=np.float32)
-        batch_y = np.empty((length, vocab_size), dtype=np.int32)
+        batch_image_input = np.empty((length, img_size, img_size, 3), dtype=np.float32)
+        batch_target = np.empty((length, vocab_size), dtype=np.int32)
         text_input = []
 
         for i_batch in range(length):
@@ -56,19 +56,20 @@ class DataGenSequence(Sequence):
             image_id = sample['image_id']
             filename = os.path.join(self.image_folder, image_id)
             image = cv.imread(filename)
-            image = cv.resize(image, (256, 256), cv.INTER_CUBIC)
-            image = random_crop(image)
+            image = cv.resize(image, (img_size, img_size), cv.INTER_CUBIC)
             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-            if np.random.random_sample() > 0.5:
-                image = np.fliplr(image)
             batch_image_input[i_batch] = image
 
+            image_input = np.array(self.image_encoding[image_id])
+            batch_image_input[i_batch] = image_input
+
             text_input.append(sample['input'])
-            batch_y[i_batch] = keras.utils.to_categorical(sample['output'], vocab_size)
+            batch_target[i_batch] = keras.utils.to_categorical(sample['output'], vocab_size)
 
         batch_text_input = sequence.pad_sequences(text_input, maxlen=max_token_length, padding='post')
         batch_image_input = preprocess_input(batch_image_input)
-        return [batch_image_input, batch_text_input], batch_y
+        batch_image_input = self.image_model.predict(batch_image_input)
+        return [batch_image_input, batch_text_input], batch_target
 
     def on_epoch_end(self):
         np.random.shuffle(self.samples)
--- model.py
+++ model.py
@@ -1,35 +1,59 @@
 import keras.backend as K
 import tensorflow as tf
-from keras.applications.vgg16 import VGG16
-from keras.layers import Input, Embedding, Dense, LSTM, TimeDistributed, RepeatVector, Concatenate, Bidirectional, Reshape
+from keras.layers import Input, Embedding, Dense, CuDNNLSTM, TimeDistributed, RepeatVector, Concatenate, Reshape, \
+    multiply, Permute, Lambda
 from keras.models import Model
 from keras.utils import plot_model
 
-from config import img_rows, img_cols, channel, max_token_length, vocab_size, embedding_size, hidden_size, L
+from config import max_token_length, vocab_size, embedding_size, hidden_size, L, D
+
+
+def generate_alphas(contexts, text_embedding):
+    # tile and concatenate inputs
+    contexts = Reshape([L * D])(contexts)
+    print(K.int_shape(contexts))
+    text_embedding = Reshape([max_token_length * embedding_size])(text_embedding)
+    print(K.int_shape(text_embedding))
+    concat_input = Concatenate(axis=-1)([contexts, text_embedding])
+
+    # feed into MLP
+    x = Dense(64, activation='relu')(concat_input)
+    x = Dense(512, activation='relu')(x)
+    x = Dense(L, activation='softmax', name='alpha_output')(x)
+    return x
 
 
 def build_model():
     # word embedding
     text_input = Input(shape=(max_token_length,), dtype='int32')
     x = Embedding(input_dim=vocab_size, output_dim=embedding_size)(text_input)
-    x = LSTM(256, return_sequences=True)(x)
+    x = CuDNNLSTM(256, return_sequences=True)(x)
     text_embedding = TimeDistributed(Dense(embedding_size))(x)
 
     # image embedding
-    image_encoder = VGG16(input_shape=(img_rows, img_cols, channel), include_top=False, weights='imagenet',
-                          pooling=None)
-    image_input = image_encoder.layers[0].input
-    x = image_encoder.layers[-2].output
-    x = Reshape([L, 512])(x)
-    x = Dense(embedding_size, activation='relu', name='image_embedding')(x)
-    # the image I is only input once
+    image_input = Input(shape=(14, 14, 512))
+    x = image_input
+    contexts = Reshape([L, D])(x)
+
+    alphas = generate_alphas(contexts, text_embedding)
+    alpha_output = alphas
+    alphas = RepeatVector(1)(alphas)
+    alphas = Permute((2, 1))(alphas)
+    x = multiply([contexts, alphas])
+    print(K.int_shape(x))
+    x = Lambda(lambda x: K.sum(x, axis=1), output_shape=lambda s: (s[0], s[2]), name='image_embedding')(x)
+    print(K.int_shape(x))
+
+    # # the image I is only input once
     image_embedding = RepeatVector(1)(x)
 
     # language model
     x = [image_embedding, text_embedding]
     x = Concatenate(axis=1)(x)
-    x = Bidirectional(LSTM(hidden_size, return_sequences=False))(x)
-    model = Model(inputs=image_input, outputs=x)
+    x = CuDNNLSTM(1024, return_sequences=True)(x)
+    x = CuDNNLSTM(1024, return_sequences=False)(x)
+    caption_output = Dense(vocab_size, activation='softmax', name='caption_output')(x)
+    model = Model(inputs=[image_input, text_input], outputs=[caption_output, alpha_output])
     return model
 
 
