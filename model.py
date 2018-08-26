c5c9302dfe9883bf2fb3a9daacc967c27b32185f
Yang Liu <foamliu@yeah.net>
2018-08-26 21:20:43 +0800

    update

--- model.py
+++ model.py
@@ -106,7 +106,6 @@ def language_model(wh, dim, convfeats, prev_words):
         atten_out = Add(name='mlp_in')([h_out_linear, c_vec])
         h = TimeDistributed(Dense(emb_dim, activation='tanh'))(atten_out)
 
-    h = Lambda(lambda x: x[:, -1, :])(h)
     predictions = Dense(vocab_size, activation='softmax', name='output')(h)
 
     model = Model(inputs=[convfeats, prev_words], outputs=predictions)
