2cdff1220da0e1019e8ddfdf59efccbdfeed5ed1
Yang Liu <foamliu@yeah.net>
2018-08-26 19:00:01 +0800

    feed images

--- data_generator.py
+++ data_generator.py
@@ -1,12 +1,22 @@
 # encoding=utf-8
+import os
 import pickle
 
 import keras
 import numpy as np
 from keras.preprocessing import sequence
+from keras.preprocessing.image import (load_img, img_to_array)
 from keras.utils import Sequence
 
-from config import batch_size, max_token_length, vocab_size, train_image_folder, valid_image_folder, L
+from config import image_h, image_w, batch_size, max_token_length, vocab_size, train_image_folder, valid_image_folder, \
+    cnn_type
+
+if cnn_type == 'vgg16':
+    from keras.applications.vgg16 import preprocess_input as preprocess_input
+elif cnn_type == 'vgg19':
+    from keras.applications.vgg19 import preprocess_input as preprocess_input
+elif cnn_type == 'resnet':
+    from keras.applications.resnet50 import preprocess_input as preprocess_input
 
 
 class DataGenSequence(Sequence):
@@ -17,16 +27,11 @@ class DataGenSequence(Sequence):
         self.idx2word = sorted(vocab)
         self.word2idx = dict(zip(self.idx2word, range(len(vocab))))
 
-        filename = 'data/encoded_{}_images.p'.format(usage)
-        self.image_encoding = pickle.load(open(filename, 'rb'))
-
         if usage == 'train':
             samples_path = 'data/samples_train.p'
             self.image_folder = train_image_folder
-            self.image_folder = train_image_folder
         else:
             samples_path = 'data/samples_valid.p'
-            self.image_folder = valid_image_folder
             self.image_folder = valid_image_folder
 
         samples = pickle.load(open(samples_path, 'rb'))
@@ -39,23 +44,24 @@ class DataGenSequence(Sequence):
     def __getitem__(self, idx):
         i = idx * batch_size
 
-        length = min(batch_size, (len(self.samples) - i))
-        batch_image_input = np.empty((length, 7, 7, 512), dtype=np.float32)
-        caption_target = np.empty((length, vocab_size), dtype=np.int32)
-        alpha_target = np.zeros((length, L), dtype=np.float32)
+        batch_image_input = np.empty((batch_size, image_h, image_w, 3), dtype=np.float32)
+        caption_target = np.empty((batch_size, vocab_size), dtype=np.int32)
         text_input = []
 
-        for i_batch in range(length):
+        for i_batch in range(batch_size):
             sample = self.samples[i + i_batch]
             image_id = sample['image_id']
-            image_input = np.array(self.image_encoding[image_id])
-            batch_image_input[i_batch] = image_input
+            filename = os.path.join(self.image_folder, str(image_id) + '.jpg')
+            img = load_img(filename, target_size=(image_h, image_w))
+            img_array = img_to_array(img)
+            img_array = preprocess_input(img_array)
+            batch_image_input[i_batch] = img_array
 
             text_input.append(sample['input'])
             caption_target[i_batch] = keras.utils.to_categorical(sample['output'], vocab_size)
 
         batch_text_input = sequence.pad_sequences(text_input, maxlen=max_token_length, padding='post')
-        return [batch_image_input, batch_text_input], [caption_target, alpha_target]
+        return [batch_image_input, batch_text_input], caption_target
 
     def on_epoch_end(self):
         np.random.shuffle(self.samples)
