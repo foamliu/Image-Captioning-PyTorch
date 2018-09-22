ed35b085af760d11dddefe45167f8284573113e6
Yang Liu <foamliu@microsoft.com>
2018-09-22 17:54:20 +0800

    update

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+import torch
+import torch.backends.cudnn as cudnn
+
 image_h = image_w = image_size = 224
 channel = 3
 batch_size = 1
@@ -7,18 +10,34 @@ epochs = 10000
 patience = 10
 num_train_samples = 1050000
 num_valid_samples = 150000
-embedding_size = 512
 vocab_size = 17628
 max_token_length = 40
-hidden_size = 512
-cnn_type = 'resnet'
-emb_dim = 512
-z_dim = 512
-lstm_dim = 512
-attlstm = True
-cnn_train = False
-finetune_start_layer = 6
 
+# Model parameters
+emb_dim = 512  # dimension of word embeddings
+attention_dim = 512  # dimension of attention linear layers
+decoder_dim = 512  # dimension of decoder RNN
+dropout = 0.5
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
+cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
+
+# Training parameters
+start_epoch = 0
+epochs = 120  # number of epochs to train for (if early stopping is not triggered)
+epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
+batch_size = 32
+workers = 1  # for data-loading; right now, only 1 works with h5py
+encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
+decoder_lr = 4e-4  # learning rate for decoder
+grad_clip = 5.  # clip gradients at an absolute value of
+alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as in the paper
+best_bleu4 = 0.  # BLEU-4 score right now
+print_freq = 100  # print training/validation stats every __ batches
+fine_tune_encoder = False  # fine-tune encoder?
+checkpoint = None  # path to checkpoint, None if none
+
+# Data parameters
+data_folder = 'data'
 train_folder = 'data/ai_challenger_caption_train_20170902'
 valid_folder = 'data/ai_challenger_caption_validation_20170910'
 test_a_folder = 'data/ai_challenger_caption_test_a_20180103'
@@ -32,4 +51,4 @@ valid_annotations_filename = 'caption_validation_annotations_20170910.json'
 
 start_word = '<start>'
 stop_word = '<end>'
-unknown_word = '<UNK>'
+unknown_word = '<unk>'
--- train.py
+++ train.py
@@ -1,42 +1,17 @@
+import json
 import time
-import torch.backends.cudnn as cudnn
+
 import torch.optim
 import torch.utils.data
 import torchvision.transforms as transforms
+from nltk.translate.bleu_score import corpus_bleu
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence
+
+from config import *
+from data_generator import CaptionDataset
 from models import Encoder, DecoderWithAttention
-from data_generator import DataGenSequence
 from utils import *
-from nltk.translate.bleu_score import corpus_bleu
-import json
-
-# Data parameters
-data_folder = '/media/ssd/caption data'  # folder with data files saved by create_input_files.py
-data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
-
-# Model parameters
-emb_dim = 512  # dimension of word embeddings
-attention_dim = 512  # dimension of attention linear layers
-decoder_dim = 512  # dimension of decoder RNN
-dropout = 0.5
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
-cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
-
-# Training parameters
-start_epoch = 0
-epochs = 120  # number of epochs to train for (if early stopping is not triggered)
-epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
-batch_size = 32
-workers = 1  # for data-loading; right now, only 1 works with h5py
-encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
-decoder_lr = 4e-4  # learning rate for decoder
-grad_clip = 5.  # clip gradients at an absolute value of
-alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as in the paper
-best_bleu4 = 0.  # BLEU-4 score right now
-print_freq = 100  # print training/validation stats every __ batches
-fine_tune_encoder = False  # fine-tune encoder?
-checkpoint = None  # path to checkpoint, None if none
 
 
 def main():
