49c8be45e983d6dc17b033ddf5fd89d2cb88fcf9
Yang Liu <foamliu@yeah.net>
2018-09-27 13:57:59 +0800

    update

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import json
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = 'WenQuanYi Micro Hei Mono'
 import numpy as np
 import skimage.transform
