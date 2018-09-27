7b6d3a8e06546f0ff7041c58b3e81cd889c9da73
Yang Liu <foamliu@yeah.net>
2018-09-27 13:57:00 +0800

    update

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import json
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.sans-serif'] = ['SimHei']  # for Windows
+plt.rcParams['font.sans-serif'] = 'WenQuanYi Micro Hei Mono'
 import numpy as np
 import skimage.transform
 import torch
